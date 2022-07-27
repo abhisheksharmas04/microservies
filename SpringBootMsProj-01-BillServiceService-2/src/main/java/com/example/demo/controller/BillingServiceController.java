2c5015c79f97048d78f6b7e51e17787a272e83dc
Abhishek <Abhishek@Abhishek>
2022-07-27 15:07:12 +0530

    Load balancer code added

--- SpringBootMsProj-01-BillServiceService-2/src/main/java/com/example/demo/controller/BillingServiceController.java
+++ SpringBootMsProj-01-BillServiceService-2/src/main/java/com/example/demo/controller/BillingServiceController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/billing")
 public class BillingServiceController {
+	
+	@Value("${server.port}")
+	private int port;
+	
+	@Value("${eureka.instance.instance-id}")
+	private String instaceId;
 
 	@GetMapping("/info")
 	public String getBillInfo() {
-		return "We accept card payment, UPI payment, Net Banking payment and COD";
+		return "We accept card payment, UPI payment, Net Banking payment and COD--port" + port + " instanceId: " + instaceId;
 	}
 }
--- SpringBootMsProj-01-Shopping-Service3/src/main/java/com/example/demo/client/BillingServiceRestConsumer.java
+++ SpringBootMsProj-01-Shopping-Service3/src/main/java/com/example/demo/client/BillingServiceRestConsumer.java
@@ -6,25 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.hypermedia.DiscoveredResource;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BillingServiceRestConsumer {
 	
-	@Autowired
+	/*@Autowired
 	private DiscoveryClient discoveryClient;
+	*/
+	// Using Netflix LoadBalancer
+	@Autowired
+	private LoadBalancerClient lbc;
+	
+	/*	public String getBillingInfo() {
+			// find and get service instance of producer by using service Id
+			List<ServiceInstance>serviceInstanceList = lbc.choose("Billing-Service");
+			// use first service instance from the list of instances
+			ServiceInstance si = serviceInstanceList.get(0);
+			//get Producer MS URI and make it url
+			String url = si.getUri() + "/billing/info";
+			//create rest template object
+			RestTemplate template = new RestTemplate();
+			//invoke producer MS service method or operation by generating Http call
+			String res = template.getForObject(url,String.class);
+			return res;
+		}*/
 	
 	public String getBillingInfo() {
-		// find and get service instance of producer by using service Id
-		List<ServiceInstance>serviceInstanceList = discoveryClient.getInstances("Billing-Service");
-		// use first service instance from the list of instances
-		ServiceInstance si = serviceInstanceList.get(0);
-		//get Producer MS URI and make it url
-		String url = si.getUri() + "/billing/info";
-		//create rest template object
+		ServiceInstance serviceInstance = lbc.choose("Billing-Service");
+		String url = serviceInstance.getUri() + "/billing/info";
 		RestTemplate template = new RestTemplate();
-		//invoke producer MS service method or operation by generating Http call
 		String res = template.getForObject(url,String.class);
 		return res;
 	}
